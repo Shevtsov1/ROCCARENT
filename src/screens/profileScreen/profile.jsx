280827f182a24ac0f3adb55214ede9f78b75125c
VSHEVTSOV <71493953+Shevtsov1@users.noreply.github.com>
2024-04-29 11:36:02 +0300

    Закончен UI кнопки выхода из аккаунта на profileMainCard

--- src/screens/profileScreen/profile.jsx
+++ src/screens/profileScreen/profile.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from "react-native";
 import { Button, Icon } from "@rneui/base";
-import {Overlay} from "@rneui/themed";
+import { Overlay } from "@rneui/themed";
 import auth from "@react-native-firebase/auth";
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
 import { SafeAreaView } from "react-native-safe-area-context";
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from "react-native-responsive-screen";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
 import { ShadowedView, shadowStyle } from "react-native-fast-shadow";
 
 
@@ -33,8 +33,8 @@ const Profile = ({ user, theme, toggleMode, navigation }) => {
       backgroundColor: theme.colors.secondary,
     }, profileMainCardContainer: {
       marginTop: hp(1),
-      justifyContent: 'center',
-      alignSelf: 'center',
+      justifyContent: "center",
+      alignSelf: "center",
     }, profileMainCard: {
       width: wp(90),
       height: 288,
@@ -43,9 +43,9 @@ const Profile = ({ user, theme, toggleMode, navigation }) => {
       borderRadius: 15,
     }, profileMainCardHeader: {
       flex: 1,
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      borderBottomWidth:1,
+      flexDirection: "row",
+      justifyContent: "space-between",
+      borderBottomWidth: 1,
     }, logoutOverlay: {
       backgroundColor: theme.colors.background,
       borderRadius: 15,
@@ -53,14 +53,14 @@ const Profile = ({ user, theme, toggleMode, navigation }) => {
       maxWidth: wp(92),
     }, profileMainCardBody: {
       flex: 4,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderBottomWidth:1,
+      justifyContent: "center",
+      alignItems: "center",
+      borderBottomWidth: 1,
     }, profileMainCardUnder: {
       flex: 3,
-      justifyContent: 'center',
-      alignItems: 'center',
-    }
+      justifyContent: "center",
+      alignItems: "center",
+    },
 
     /* BODY END */
   });
@@ -73,41 +73,65 @@ const Profile = ({ user, theme, toggleMode, navigation }) => {
             color: theme.colors.text,
             opacity: 0.8,
             radius: 4,
-            offset: [0,0]
-          }), {borderRadius: 15}]}>
+            offset: [0, 0],
+          }), { borderRadius: 15 }]}>
             <View style={styles.profileMainCard}>
               <View style={styles.profileMainCardHeader}>
                 <TouchableOpacity>
-                  <Icon type={"ionicon"} name={'settings-outline'} size={24} color={theme.colors.text}/>
+                  <Icon type={"ionicon"} name={"settings-outline"} size={24} color={theme.colors.text} />
                 </TouchableOpacity>
                 <TouchableOpacity onPress={toggleOverlay}>
-                  <Icon type={"ionicon"} name={'log-out-outline'} size={28} color={theme.colors.text}/>
+                  <Icon type={"ionicon"} name={"log-out-outline"} size={28} color={theme.colors.text} />
                 </TouchableOpacity>
                 <Overlay overlayStyle={styles.logoutOverlay} isVisible={visible} onBackdropPress={toggleOverlay}>
-                  <ShadowedView style={[{alignSelf: 'center', borderRadius: 15,}, shadowStyle({
+                  <ShadowedView style={[{ alignSelf: "center", borderRadius: 15 }, shadowStyle({
                     color: theme.colors.error,
-                    opacity: 0.4,
-                    radius: 12,
-                    offset: [0,0]
+                    opacity: 0.3,
+                    radius: 15,
+                    offset: [0, 2],
                   })]}>
-                    <Icon type={"ionicon"} name={'information-circle-outline'} color={theme.colors.error}/>
+                    <Icon type={"ionicon"} name={"warning-outline"} color={theme.colors.error} size={36} />
                   </ShadowedView>
-                  <View>
-                    <Text style={{fontFamily: 'Roboto-Medium', color: theme.colors.text}}>
+                  <View style={{ alignSelf: "center" }}>
+                    <Text
+                      style={{ fontFamily: "Roboto-Black", fontSize: 18, color: theme.colors.text, marginBottom: 12 }}>
                       Выход из аккаунта
                     </Text>
                   </View>
-                  <View>
-                    <Text style={{fontFamily: 'Roboto-Medium', color: theme.colors.text}}>
-                      Вы уверены что хотите выйти из аккаунта
+                  <View style={{ alignSelf: "center" }}>
+                    <Text style={{ fontFamily: "Roboto-Medium", fontSize: 14, color: theme.colors.text }}>
+                      Вы уверены что хотите выйти?
+                    </Text>
+                    <Text style={{
+                      fontFamily: "Roboto-Medium",
+                      fontSize: 12,
+                      color: theme.colors.text,
+                      alignSelf: "center",
+                      marginBottom: 12,
+                    }}>
+                      (Ваши данные будут сохранены)
                     </Text>
                   </View>
-                  <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                    <TouchableOpacity style={{width: '46%', borderRadius: 15, backgroundColor: theme.colors.grey2, alignItems: 'center'}} onPress={toggleOverlay}>
-                      <Text style={{fontFamily: 'Roboto-Medium', color: theme.colors.accentText}}>Назад</Text>
+                  <View style={{ flexDirection: "row", justifyContent: "space-between"}}>
+                    <TouchableOpacity style={{
+                      width: "46%",
+                      height: 36,
+                      borderRadius: 15,
+                      backgroundColor: theme.colors.grey2,
+                      alignItems: "center",
+                      justifyContent: 'center'
+                    }} onPress={toggleOverlay}>
+                      <Text style={{ fontFamily: "Roboto-Bold", color: theme.colors.accentText, fontSize: 16 }}>Назад</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={{width: '46%', borderRadius: 15, backgroundColor: theme.colors.error, alignItems: 'center'}} onPress={toggleOverlay}>
-                      <Text style={{fontFamily: 'Roboto-Medium', color: theme.colors.accentText}}>Выйти</Text>
+                    <TouchableOpacity style={{
+                      width: "46%",
+                      height: 36,
+                      borderRadius: 15,
+                      backgroundColor: theme.colors.error,
+                      alignItems: "center",
+                      justifyContent: 'center'
+                    }} onPress={toggleOverlay}>
+                      <Text style={{ fontFamily: "Roboto-Bold", color: theme.colors.accentText, fontSize: 16 }}>Выйти</Text>
                     </TouchableOpacity>
                   </View>
                 </Overlay>
