443e2cb59ccd72c99c5429da21afca3af5caff83
VSHEVTSOV <71493953+Shevtsov1@users.noreply.github.com>
2024-05-09 15:14:46 +0300

    Готовый ui и локальное сохранение passportData

--- src/screens/profileScreen/components/editProfile/editPassport.jsx
+++ src/screens/profileScreen/components/editProfile/editPassport.jsx
@@ -6,14 +6,17 @@ import { Button } from "@rneui/base";
 import TextRecognition from "@react-native-ml-kit/text-recognition";
 import { useCameraDevice, useCameraPermission, Camera } from "react-native-vision-camera";
 import FastImage from "react-native-fast-image";
+import { launchImageLibrary } from "react-native-image-picker";
 
 const EditPassport = ({ theme, navigation }) => {
 
-  const [isCameraActive, setCameraActive] = useState(true);
+  const [isCameraActive, setCameraActive] = useState(false);
   const device = useCameraDevice("back");
   const { hasPermission, requestPermission } = useCameraPermission();
   const camera = useRef(null);
   const [loadingPhoto, setLoadingPhoto] = useState(false);
+  const [isTakingPhotoFromCamera, setTakingPhotoFromCamera] = useState(false);
+  const [isTakingPhotoFromCameraLibrary, setTakingPhotoFromCameraLibrary] = useState(false);
 
   const [passportPhoto, setPassportPhoto] = useState("");
   const [passportData, setPassportData] = useState("");
@@ -26,16 +29,57 @@ const EditPassport = ({ theme, navigation }) => {
     }
   }, []);
 
+  useEffect(() => {
+    const visibleText = passportData.slice(0, 3);
+    const hiddenText = passportData.slice(3, -1).replace(/./g, "*");
+    const lastVisibleChar = passportData.slice(-1);
+    setPassportData(visibleText + hiddenText + lastVisibleChar);
+  }, [passportData]);
+
+  const handleOpenCamera = async () => {
+    setPassportPhoto("");
+    setCameraActive(true);
+  };
+
   const handleTakePhotoBtn = async () => {
-    setLoadingPhoto(true);
+    setTakingPhotoFromCamera(true);
     const photo = await camera.current.takePhoto({
-      flash: "auto",
-      enableShutterSound: "false",
-      enableAutoRedEyeReduction: true,
+      flash: "auto", enableShutterSound: "false", enableAutoRedEyeReduction: true,
     });
     setPassportPhoto("file://" + photo.path);
     setCameraActive(false); // Останавливаем камеру
-    setLoadingPhoto(false);
+    setTakingPhotoFromCamera(false);
+  };
+
+  const handleTakePhotoFromLibraryBtn = async () => {
+    setTakingPhotoFromCameraLibrary(true);
+    try {
+      const options = {
+        title: "Выберите изображение",
+        cancelButtonTitle: "Отмена",
+        takePhotoButtonTitle: "Выбрать изображение",
+        quality: 1,
+        mediaType: "photo",
+        storageOptions: {
+          skipBackup: true, path: "images",
+        },
+        selectionLimit: 1,
+      };
+      const selectedImage = await launchImageLibrary(options, (response) => {
+        if (response.didCancel) {
+          console.log("Пользователь отменил съемку фотографии");
+        } else if (response.error) {
+          console.log("Ошибка съемки фотографии:", response.error);
+        }
+      });
+      if (selectedImage && selectedImage.assets[0]) {
+        setPassportPhoto(selectedImage.assets[0].uri);
+        setCameraActive(false);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+    setTakingPhotoFromCameraLibrary(false);
   };
 
   const verifyPassport = async (passportImageUri) => {
@@ -48,21 +92,18 @@ const EditPassport = ({ theme, navigation }) => {
     }
     for (let i = 0; i < finalArray.length; i++) {
       const element = finalArray[i];
-      if (passportRegexPattern.test(element)) {
+      if (passportRegexPattern.test(element.trim())) {
         setPassportData(element);
         setPassportVerified(true);
       }
     }
-    if (!passportVerified) {
-      setPassportVerified(false);
-      setPassportPhoto('');
-      setCameraActive(true);
-      ToastAndroid.show('Не удалось просканировать пасспорт, повторите попытку', 7000);
+    if (passportVerified) {
+      ToastAndroid.show("Паспорт успешно подтвержден", 7000);
+      // navigation.navigate("Profile");
     } else {
-      setPassportVerified(true)
-      setLoadingPhoto('');
-      ToastAndroid.show('Паспорт успешно подтвержден', 7000);
-      navigation.navigate('Profile');
+      setPassportPhoto("");
+      setCameraActive(true);
+      ToastAndroid.show("Не удалось просканировать пасспорт, повторите попытку", 7000);
     }
   };
 
@@ -70,15 +111,11 @@ const EditPassport = ({ theme, navigation }) => {
     setLoadingPhoto(true);
     await verifyPassport(passportPhoto);
     setLoadingPhoto(false);
-  }
-
-  const handleSubmitBtn = () => {
   };
 
   const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: theme.colors.background,
+      flex: 1, backgroundColor: theme.colors.background,
     }, editNameContainer: {
       width: "100%",
       height: "auto",
@@ -87,7 +124,7 @@ const EditPassport = ({ theme, navigation }) => {
       borderBottomStartRadius: 15,
       borderBottomEndRadius: 15,
     }, header: {
-      height: 36,
+      height: 60,
       paddingHorizontal: 12,
       backgroundColor: theme.colors.accent,
       flexDirection: "row",
@@ -96,65 +133,64 @@ const EditPassport = ({ theme, navigation }) => {
     }, headerBackBtn: {
       justifyContent: "center",
     }, headerBackBtnText: {
-      fontFamily: "Roboto-Regular",
-      fontSize: 16,
-      color: theme.colors.accentText,
-      alignSelf: "center",
+      fontFamily: "Roboto-Regular", fontSize: 16, color: theme.colors.accentText, alignSelf: "center",
     }, infoText: {
-      width: "100%",
-      fontFamily: "Roboto-Regular",
-      fontSize: 14,
-      color: theme.colors.grey1,
-    }, inputComponentContainer: {
-      height: 72,
-      alignSelf: "center",
-      marginBottom: 12,
-    }, inputContainer: {
-      borderWidth: 1,
-      borderColor: theme.colors.accent,
-      borderRadius: 5,
-    }, inputTextStyle: {
-      fontFamily: "Roboto-Regular",
-      fontSize: 16,
-      color: theme.colors.text,
+      width: "100%", fontFamily: "Roboto-Regular", fontSize: 14, color: theme.colors.grey1,
     }, submitBtnContainer: {
-      borderRadius: 5,
-      marginHorizontal: wp(2),
+      borderRadius: 5, marginHorizontal: wp(2),
     }, submitBtn: {
-      width: "100%",
-      height: 48,
-      backgroundColor: theme.colors.accent,
+      width: "100%", height: 48, backgroundColor: theme.colors.accent,
     },
   });
-  return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <View style={{ paddingBottom: hp(1) }}>
-          <View style={styles.editNameContainer}>
-            <View style={styles.header}>
-              <Text
-                style={{
-                  fontFamily: "Roboto-Medium",
-                  fontSize: 18,
-                  color: theme.colors.accentText,
-                  alignSelf: "center",
-                }}>Подтверждение
-                Паспорта</Text>
-              <TouchableOpacity style={styles.headerBackBtn} onPress={() => navigation.goBack()}>
-                <Text style={styles.headerBackBtnText}>Назад</Text>
-              </TouchableOpacity>
-            </View>
-            <View style={{ paddingHorizontal: 12, marginVertical: 12, flex: 1 }}>
-              <Text style={styles.infoText}>
-                Для подтверждения паспорта, пожалуйста, загрузите фотографию паспорта ниже.
-                Мы гарантируем конфиденциальность ваших данных и обеспечиваем их безопасность.
+
+  return (<SafeAreaView style={styles.container}>
+    <ScrollView>
+      <View style={{ paddingBottom: hp(1) }}>
+        <View style={styles.editNameContainer}>
+          <View style={styles.header}>
+            <Text
+              style={{
+                fontFamily: "Roboto-Bold", fontSize: 18, color: theme.colors.accentText, alignSelf: "center",
+              }}>Подтверждение
+              Паспорта</Text>
+            <TouchableOpacity style={styles.headerBackBtn} onPress={() => navigation.goBack()}>
+              <Text style={styles.headerBackBtnText}>Назад</Text>
+            </TouchableOpacity>
+          </View>
+          {passportData ? (<View style={{
+              width: "100%",
+              paddingHorizontal: 12,
+              paddingVertical: 12,
+            }}>
+              <Text numberOfLines={1} style={{ fontFamily: "Roboto-Regular", fontSize: 16, color: theme.colors.text }}>
+                Ваш Паспорт подтвержден
               </Text>
-              <Text style={styles.infoText}>
-                Пожалуйста, убедитесь, что фотография является четкой и полностью воспроизводит информацию в паспорте.
+              <Text numberOfLines={1} style={{
+                fontFamily: "Roboto-Black",
+                fontSize: 32,
+                color: theme.colors.text,
+                alignSelf: "center",
+                marginTop: 12,
+              }}>
+                {passportData}
               </Text>
+              <FastImage style={{ width: wp(50), height: wp(50), alignSelf: 'center', marginTop: hp(1) }}
+                         source={require("../../../../assets/images/passport.png")}
+                         resizeMode={FastImage.resizeMode.contain} />
             </View>
-            {hasPermission &&
-              <View style={{
+          ) : (
+            <View>
+              <View style={{ paddingHorizontal: 12, marginVertical: 12, flex: 1 }}>
+                <Text style={styles.infoText}>
+                  Для подтверждения паспорта, пожалуйста, загрузите фотографию паспорта ниже.
+                  Мы гарантируем конфиденциальность ваших данных и обеспечиваем их безопасность.
+                </Text>
+                <Text style={styles.infoText}>
+                  Пожалуйста, убедитесь, что фотография является четкой и полностью воспроизводит информацию в
+                  паспорте.
+                </Text>
+              </View>
+              {((isCameraActive && hasPermission) || passportPhoto) && <View style={{
                 width: wp(90),
                 height: hp(30),
                 borderWidth: 1,
@@ -162,25 +198,49 @@ const EditPassport = ({ theme, navigation }) => {
                 alignSelf: "center",
                 marginBottom: 12,
               }}>
-                {passportPhoto ? (
+                {passportPhoto && !isCameraActive ? (
                   <FastImage style={{ width: "100%", height: "100%" }} resizeMode={FastImage.resizeMode.cover}
                              source={{ uri: passportPhoto }} />) : (
-                  <Camera style={{ width: "100%", height: "100%" }} device={device} isActive={isCameraActive} photo={true}
+                  <Camera style={{ width: "100%", height: "100%" }} device={device} isActive={isCameraActive}
+                          photo={true}
                           ref={camera} />)}
               </View>}
-            <Button containerStyle={styles.submitBtnContainer} buttonStyle={styles.submitBtn}
-                    titleStyle={{ color: theme.colors.grey1 }} loading={loadingPhoto} loadingStyle={styles.submitBtn}
-                    onPress={passportPhoto ? handleVerifyPassport : handleTakePhotoBtn}>{passportPhoto ?
-              <Text style={{ fontFamily: "Roboto-Medium", fontSize: 18, color: theme.colors.accentText }}>
-                Отправить на проверку
-              </Text> : <Text
-                style={{ fontFamily: "Roboto-Medium", fontSize: 18, color: theme.colors.accentText }}>Сделать
-                снимок</Text>}</Button>
-          </View>
+              <Button containerStyle={styles.submitBtnContainer} buttonStyle={styles.submitBtn}
+                      titleStyle={{ color: theme.colors.grey1 }} loading={isTakingPhotoFromCamera}
+                      loadingStyle={styles.submitBtn}
+                      onPress={isCameraActive ? handleTakePhotoBtn : handleOpenCamera}
+                      disabled={isTakingPhotoFromCameraLibrary || loadingPhoto}><Text
+                style={{
+                  fontFamily: "Roboto-Medium",
+                  fontSize: 16,
+                  color: loadingPhoto || isTakingPhotoFromCameraLibrary ? theme.colors.greyOutline : theme.colors.accentText,
+                }}>{isCameraActive ? ("Сделать снимок") : ("Открыть камеру")}</Text></Button>
+              <Button containerStyle={[styles.submitBtnContainer, { marginTop: 12 }]} buttonStyle={styles.submitBtn}
+                      titleStyle={{ color: theme.colors.grey1 }} loading={isTakingPhotoFromCameraLibrary}
+                      loadingStyle={styles.submitBtn}
+                      onPress={handleTakePhotoFromLibraryBtn} disabled={isTakingPhotoFromCamera || loadingPhoto}><Text
+                style={{
+                  fontFamily: "Roboto-Medium",
+                  fontSize: 16,
+                  color: loadingPhoto || isTakingPhotoFromCamera ? theme.colors.greyOutline : theme.colors.accentText,
+                }}>Выбрать из
+                галереи</Text></Button>
+              <Button containerStyle={[styles.submitBtnContainer, { marginTop: 12 }]} buttonStyle={styles.submitBtn}
+                      titleStyle={{ color: theme.colors.grey1 }} loading={passportPhoto && loadingPhoto}
+                      loadingStyle={styles.submitBtn}
+                      onPress={handleVerifyPassport}
+                      disabled={!passportPhoto || isTakingPhotoFromCamera || isTakingPhotoFromCameraLibrary}><Text
+                style={{
+                  fontFamily: "Roboto-Medium",
+                  fontSize: 16,
+                  color: !passportPhoto || isTakingPhotoFromCamera || isTakingPhotoFromCameraLibrary ? theme.colors.greyOutline : theme.colors.accentText,
+                }}>Отправить на
+                проверку</Text></Button>
+            </View>)}
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+      </View>
+    </ScrollView>
+  </SafeAreaView>);
 };
 
 export default EditPassport;
