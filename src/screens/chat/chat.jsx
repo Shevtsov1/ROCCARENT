07ff5f97870337e43c5bdc78d544cb46e3cdc59a
VSHEVTSOV <71493953+Shevtsov1@users.noreply.github.com>
2024-05-16 16:18:11 +0300

    Добавлен экран чата как файл

---
+++ src/screens/chat/chat.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Chat = ({theme, messages, handleSendMessage, handleChangeMessage}) => {
+  return (
+    <View>
+      <Text>Chat</Text>
+    </View>
+  );
+};
+
+export default Chat;
