89f41d954d7ac55df00e22963662b7fafd5e96cd
VSHEVTSOV <71493953+Shevtsov1@users.noreply.github.com>
2024-05-20 12:06:56 +0300

    UI для открытого чата

--- src/screens/chat/openedChat.jsx
+++ src/screens/chat/openedChat.jsx
@@ -1,33 +1,92 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Image, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Avatar } from "@rneui/themed";
 import { AppContext } from "../../../App";
 
-const OpenedChat = ({ theme, navigation}) => {
+const OpenedChat = ({ theme, navigation }) => {
 
-  const {userdata} = useContext(AppContext);
+  const { userdata } = useContext(AppContext);
 
   const [isSendBtnDisabled, setSendBtnDisabled] = useState(true);
+  const [messages, setMessages] = useState([]);
+  const [messageToSend, setMessageToSend] = useState(null);
+
+  useEffect(() => {
+    console.log(messageToSend)
+  }, [messageToSend]);
+
+  const verifyMessageToSend = (value) => {
+    return value.length !== 0
+  };
+
+  const handleMessageToSend = (value) => {
+    setMessageToSend(value);
+
+    if (verifyMessageToSend(value)) {
+      setSendBtnDisabled(false);
+    } else {
+      setSendBtnDisabled(true);
+    }
+  };
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View style={{flex: 1, backgroundColor: theme.colors.background}}>
-        <View style={{height: 60, backgroundColor: theme.colors.accent, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 6}}>
-          <Avatar containerStyle={{ marginEnd: 6 }} size={'medium'} rounded
+      <View style={{ flex: 1, backgroundColor: theme.colors.background }}>
+        <View style={{
+          height: 60,
+          backgroundColor: theme.colors.accent,
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          paddingHorizontal: 6,
+        }}>
+          <Avatar containerStyle={{ marginEnd: 6 }} size={"medium"} rounded
                   source={userdata && userdata.photoUrl ? { uri: userdata.photoUrl } : require("../../assets/images/save.png")} />
-          <Text style={{fontFamily: 'Roboto-Medium', fontSize: 18, color: theme.colors.accentText}}>Name</Text>
+          <Text style={{ fontFamily: "Roboto-Medium", fontSize: 18, color: theme.colors.accentText }}>Name</Text>
           <TouchableOpacity onPress={() => navigation.goBack()}>
-            <Text style={{fontFamily: 'Roboto-Regular', fontSize: 16, color: theme.colors.accentText}}>Назад</Text>
+            <Text style={{ fontFamily: "Roboto-Regular", fontSize: 16, color: theme.colors.accentText }}>Назад</Text>
           </TouchableOpacity>
         </View>
         <ScrollView>
-          <Text>Chat</Text>
+          {messages.length !== 0 ? <View>
+              {messages.map((message, index) => (
+                <Text key={index}>{message}</Text>
+              ))}
+            </View>
+            :
+            <Text style={{
+              fontFamily: "Roboto-Regular",
+              fontSize: 14,
+              color: theme.colors.text,
+              alignSelf: "center",
+              justifyContent: "center",
+              marginTop: 12,
+            }}>Нет сообщений</Text>}
         </ScrollView>
-        <View style={{marginBottom: 12, marginHorizontal: 6, paddingHorizontal: 12, borderRadius: 15, borderWidth: 2, borderColor: theme.colors.accent, alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between'}}>
-          <TextInput maxLength={2500} multiline style={{height: 36, width: '90%',  borderRadius: 15, paddingVertical: 0,  backgroundColor: theme.colors.background}}/>
+        <View style={{
+          marginBottom: 12,
+          marginHorizontal: 6,
+          paddingHorizontal: 12,
+          borderRadius: 15,
+          borderWidth: 2,
+          borderColor: theme.colors.accent,
+          alignItems: "center",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}>
+          <TextInput maxLength={2500} multiline style={{
+            height: 36,
+            width: "90%",
+            borderRadius: 15,
+            paddingVertical: 0,
+            backgroundColor: theme.colors.background,
+          }} value={messageToSend}
+                     onChangeText={value => handleMessageToSend(value)} />
           <TouchableOpacity disabled={isSendBtnDisabled}>
-            <Image source={isSendBtnDisabled ? require('../../assets/images/sender-outline.png') : require('../../assets/images/sender.png')} style={{width: 24, height: 24, tintColor: theme.colors.accent}}/>
+            <Image
+              source={isSendBtnDisabled ? require("../../assets/images/sender-outline.png") : require("../../assets/images/sender.png")}
+              style={{ width: 24, height: 24, tintColor: theme.colors.accent }} />
           </TouchableOpacity>
         </View>
       </View>
