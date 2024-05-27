48f91c501714116351db761046b2a6cc05e4452f
VSHEVTSOV <71493953+Shevtsov1@users.noreply.github.com>
2024-05-27 15:49:46 +0300

    Добавлен макет UI для catalog.jsx

--- src/screens/catalogScreen/catalog.jsx
+++ src/screens/catalogScreen/catalog.jsx
@@ -1,8 +1,52 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useContext, useEffect, useState} from 'react';
+import {View, Text, StyleSheet, TextInput, InputAccessoryView, TouchableOpacity} from 'react-native';
+import CardsGrid from "../../components/cardsGrid";
+import firestore from "@react-native-firebase/firestore";
+import auth from "@react-native-firebase/auth";
+import {AppContext} from "../../../App";
+import {SafeAreaView} from "react-native-safe-area-context";
+import {Icon, SearchBar} from "@rneui/base";
 
-const Catalog = () => {
+const Catalog = ({theme}) => {
 
+  const { loadUserdata } = useContext(AppContext);
+
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    const load = async () => {
+      await loadUserdata();
+      await loadListingList();
+    };
+    load().then();
+  }, []);
+
+  const handleReloadBtn = async () => {
+    try {
+      await loadUserdata();
+      await loadListingList();
+    } catch (error) {
+      console.log(error);
+    } finally {
+    }
+  };
+
+  const loadListingList = async () => {
+    try {
+      let newListingsArr = [];
+      const listingsData = await firestore().collection("listings").get();
+      if (listingsData) {
+        listingsData.forEach(doc => {
+          if (doc.data().ownerId !== auth().currentUser.uid) {
+            newListingsArr.push(doc.data());
+          }
+        });
+        setListings(newListingsArr);
+      }
+    } catch (error) {
+      console.error("Error fetching documents: ", error);
+    }
+  };
 
   const styles = StyleSheet.create({
 
@@ -12,17 +56,50 @@ const Catalog = () => {
       flex:1,
       justifyContent: "center",
       alignItems: "center",
+      backgroundColor: theme.colors.background,
     },
 
     /* BODY END */
   });
 
   return (
-    <View style={{flex: 1}}>
-      <View style={styles.body}>
-        <Text>C A T A L O G</Text>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{flexDirection: 'row', backgroundColor: theme.colors.background}}>
+        <SearchBar
+            containerStyle={{flex: 1, backgroundColor: theme.colors.background, borderTopWidth: 0, borderBottomWidth: 0}}
+            inputContainerStyle={{borderColor: theme.colors.accent, borderRadius: 5, borderBottomWidth: 2, borderWidth: 2, backgroundColor: `${theme.colors.background}9A`}}
+            blurOnSubmit
+            searchIcon={<Icon type={'ionicon'} name={'search'} color={theme.colors.accent}/>}
+            clearIcon={<Icon type={'ionicon'} name={'close'} color={theme.colors.accent}/>}
+            placeholder="Поиск"
+            // onChangeText={updateSearch}
+            // value={search}
+        />
+        <TouchableOpacity style={{alignSelf: 'center', marginEnd: 6,}}>
+          <Icon type={'ionicon'} name={'filter'} color={theme.colors.accent}/>
+        </TouchableOpacity>
       </View>
-    </View>
+      <View style={styles.body}>
+        <View
+            style={{
+              width: "100%",
+              backgroundColor: theme.colors.background,
+              borderRadius: 15,
+              alignItems: "center",
+            }}
+        >
+          <CardsGrid
+              theme={theme}
+              items={listings}
+              likes
+              screen={"Main"}
+              reloadFunction={() => handleReloadBtn()}
+              authHint
+              // deals
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
